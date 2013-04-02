516eea343bd82a183b648e399f98b8a13f15bfb8
Wan Yechao <wyc.jar@gmail.com>
2013-04-02 15:51:04 +0800

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DouAnalytics::Application.config.secret_token = '5b267d5e39c41b35735748582daa56fc17b7cea7cdae5a9120a609b19fc86124789098616ac25783e8a0f8f10dd3fe3dff0b6b651491e95af267b6c0ac391980'
